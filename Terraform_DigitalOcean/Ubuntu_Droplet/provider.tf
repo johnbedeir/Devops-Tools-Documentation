d2a4f317015ae7f152f2f944cb220c5cbf4c7636
JohnyDev <john.bedeir@gmail.com>
2022-04-10 21:16:07 +0200

    terraform provider ubuntu digitalocean

---
+++ Terraform_DigitalOcean/Ubuntu_Droplet/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.token
+}
