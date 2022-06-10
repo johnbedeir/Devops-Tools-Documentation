1bba0f2ac7c10c3b91230709aef70f4db2f1a939
JohnyDev <john.bedeir@gmail.com>
2022-06-10 02:26:25 +0200

    update micro ec2 to medium aws terraform

--- Terraform/AWS/Ubuntu_EC2/main.tf
+++ Terraform/AWS/Ubuntu_EC2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ubuntu-instance" {
   ami           = var.ami
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name      = "mykey"
   security_groups = ["${aws_security_group.UbuntuSG.name}"]
 
