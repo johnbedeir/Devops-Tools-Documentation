50514d1287e569e63013295856702bd2cfdab749
JohnyDev <john.bedeir@gmail.com>
2022-07-18 08:51:04 +0200

    sync dir

--- DevOps-Pipeline/terraform/output.tf
+++
@@ -1,4 +0,0 @@
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.ubuntu-instance.public_ip
-}
--- DevOps-Pipeline/terraform/providers.tf
+++
@@ -1,5 +0,0 @@
-# Adding AWS credentials ans set the region 
-provider "aws" {
-  profile = "default"
-  region  = var.region
-}
