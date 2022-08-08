d4175e98bff58cacd4e86767b84258fcc91db180
JohnyDev <john.bedeir@gmail.com>
2022-08-08 20:12:21 +0200

    #96 fix docker installation sctipt.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -9,11 +9,11 @@ sudo apt-get update -y
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update -y
-sudo apt-get install python3.8
+sudo apt-get install python3.8 -y 
 #Install Pip
 echo "--------------------Installing Pip--------------------"
 sudo apt-get update -y
-sudo apt-get install python3-pip
+sudo apt-get install python3-pip -y 
 #Install Jenkins 
 echo "--------------------Installing Jenkins--------------------"
 sudo apt -y install wget
@@ -25,17 +25,20 @@ sudo apt install git
 sudo systemctl start jenkins
 #Install docker
 echo "--------------------Installing Docker--------------------"
-sudo apt-get update -y \
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
-"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 #Install ansible
 echo "--------------------Installing Ansible--------------------"
 sudo apt update -y
