190893706be1bcb1886a5221cb39166d0e5f47a3
JohnyDev <john.bedeir@gmail.com>
2022-08-14 00:11:43 +0200

    python settings allowed hosts all

--- DevOps-Pipeline-Python/Python/polling/settings.py
+++ DevOps-Pipeline-Python/Python/polling/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # DEBUG = config('DEBUG', default=False, cast=bool)
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['192.168.49.2', 'localhost', '127.0.0.1']
 
 # Application definition
