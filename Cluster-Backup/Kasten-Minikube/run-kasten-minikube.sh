d225dd9cf493454ab59d1da20c149af273728825
JohnyDev <john.bedeir@gmail.com>
2022-08-27 19:56:17 +0200

    #119-kasten-minikube-script

--- Cluster-Backup/Kasten-Minikube/run-kasten-minikube.sh
+++ Cluster-Backup/Kasten-Minikube/run-kasten-minikube.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 NAMESPACE=kasten-io
-# check_jq=snap list jq | grep latest
-NS_CHECK=$"kubectl get ns kasten-io -o jsonpath='{.status.phase}' | grep Active"
-#NS_CHECK=$"kubectl get ns kasten-io -o json | jq .status.phase -r"
-#NS_CHECK=$"kubectl describe ns kasten-io | sed '4!d' | awk '{print $2}'"
 
-if [ "$NS_CHECK" == "Active" ]; then
+# Check if minikube is running 
+echo "Check Minikube Status"
+minikube status 
+echo "If Minikube is Running press (y) to proceed, if not press (n) to start Minikube"
+read n
+# If minikube is not running then start it first 
+if [[ $n = n ]]
 
-    echo "kasten-io namespace already exist, Do you want to Delete namespace? y/n"
-    read n
-    if [[ $n = y ]]; then
+then
+
+    minikube start
+    
+    # After starting minikube check if kasten namespace exist 
+    echo "Check if kasten namespace already exist..!"
+    kubectl get ns kasten-io
+    
+    echo "If kasten-io namespace not exist press (y) to create it, or (d) to delete it"
+    read n 
+
+    # Delete kasten namespace 
+    if [[ $n = d ]]; then
 
         #Delete deployments
         echo "--------------------Delete Deployment--------------------"
@@ -48,47 +60,128 @@ if [ "$NS_CHECK" == "Active" ]; then
         #--------------------------------------------------------------
         #--------------------------------------------------------------
 
-    else
-        exit
+        #Wait for completely remove Kasten
+        echo "--------------------Wait 30s to completely remove k10--------------------"
+        sleep 30s
+    
+    # Create kasten namespace
+    elif [[ $n = y ]]; then
+
+      #Create namespace
+      echo "--------------------Create Namespace--------------------"
+      kubectl create namespace $NAMESPACE || true
+
+      #Add Helm Repo
+      echo "--------------------Add Kasten Helm Repo--------------------"
+      helm repo add kasten https://charts.kasten.io/ || true
+
+      #Install kasten with helm and port forward
+      echo "--------------------Install Kasten--------------------"
+      helm install k10 kasten/k10 --namespace=$NAMESPACE || true
+
+      #Wait for the pod to start
+      echo "--------------------Wait 1m Pod is Starting--------------------"
+      sleep 1m
+
+      #Update helm repo 
+      echo "--------------------Update Helm Repo & Upgrade K10--------------------"
+      helm repo update && \
+          helm get values k10 --output yaml --namespace=kasten-io > k10_val.yaml && \
+          helm upgrade k10 kasten/k10 --namespace=kasten-io -f k10_val.yaml || true
+
+      #Port forward
+      echo "--------------------Port Forward--------------------"
+      kubectl --namespace kasten-io port-forward service/gateway 8080:8000 &
+
+      #open kasten on brwoser
+      xdg-open http://127.0.0.1:8080/k10/
+    
     fi
 
-elif [ "$NS_CHECK" != "Active" ]; then
+  # If minikube is already running then check kasten namespace 
+  elif [[ $n = y ]]
 
-    echo "kasten-io namespace does not exist, Do you want to Create namespace? y/n"
-    read n
-    if [[ $n = y ]]; then
+  then 
 
-        #Create namespace
-        echo "--------------------Create Namespace--------------------"
-        kubectl create namespace $NAMESPACE
+    echo "Check if kasten namespace already exist..!"
+    kubectl get ns kasten-io
+    
+    echo "If kasten-io namespace not exist press (y) to create it, or (d) to delete it"
+    read n 
+    if [[ $n = d ]]; then
 
-        #Add Helm Repo
-        echo "--------------------Add Kasten Helm Repo--------------------"
-        helm repo add kasten https://charts.kasten.io/
+        #Delete deployments
+        echo "--------------------Delete Deployment--------------------"
+        kubectl delete deploy --all -n $NAMESPACE || true
 
-        #Install kasten with helm and port forward
-        echo "--------------------Install Kasten--------------------"
-        helm install k10 kasten/k10 --namespace=$NAMESPACE 
+        #Delete services
+        echo "--------------------Delete Services--------------------"
+        kubectl delete service --all -n $NAMESPACE || true
 
-        #Wait for the pod to start
-        echo "--------------------Wait 1m Pod is Starting--------------------"
-        sleep 1m
+        #Delete configmap
+        echo "--------------------Delete Configmap--------------------"
+        kubectl delete configmap --all -n $NAMESPACE || true
 
-        #Update helm repo 
-        echo "--------------------Update Helm Repo & Upgrade K10--------------------"
-        helm repo update && \
-            helm get values k10 --output yaml --namespace=kasten-io > k10_val.yaml && \
-            helm upgrade k10 kasten/k10 --namespace=kasten-io -f k10_val.yaml
+        #Delete namespace
+        echo "--------------------Delete Namespace--------------------"
+        timeout 5s kubectl delete namespace $NAMESPACE || true
 
-        #Port forward
-        echo "--------------------Port Forward--------------------"
-        kubectl --namespace kasten-io port-forward service/gateway 8080:8000
+        #--------------------------------------------------------------
+        # Enable the following 3 commands only if you cannot delete the namespace and the command 
+        # "kubectl delete namespace kasten-io" stuck 
+        #--------------------------------------------------------------
 
-        #open kasten on brwoser
-        xdg-open http://127.0.0.1:8080/k10/
+        #Get namespace json file 
+        echo "--------------------Get NS Json--------------------"
+        kubectl get namespace $NAMESPACE -o json > ns.json
 
-    else
-        exit
+        #Remove kubernetes from namespace metadata to be able to delete namespace
+        echo "--------------------Edit NS Json--------------------"
+        sed -i '/"kubernetes"/d' ./ns.json
+
+        # #replace the new json with the old one 
+        echo "--------------------Replace NS Json--------------------"
+        kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f ./ns.json
+
+        #--------------------------------------------------------------
+        #--------------------------------------------------------------
+        #--------------------------------------------------------------
+        
+        #Wait for completely remove Kasten
+        echo "--------------------Wait 30s to completely remove k10--------------------"
+        sleep 30s
+    
+    elif [[ $n = y ]]; then
+
+      #Create namespace
+      echo "--------------------Create Namespace--------------------"
+      kubectl create namespace $NAMESPACE
+
+      #Add Helm Repo
+      echo "--------------------Add Kasten Helm Repo--------------------"
+      helm repo add kasten https://charts.kasten.io/
+
+      #Install kasten with helm and port forward
+      echo "--------------------Install Kasten--------------------"
+      helm install k10 kasten/k10 --namespace=$NAMESPACE 
+
+      #Wait for the pod to start
+      echo "--------------------Wait 1m Pod is Starting--------------------"
+      sleep 1m
+
+      #Update helm repo 
+      echo "--------------------Update Helm Repo & Upgrade K10--------------------"
+      helm repo update && \
+          helm get values k10 --output yaml --namespace=kasten-io > k10_val.yaml && \
+          helm upgrade k10 kasten/k10 --namespace=kasten-io -f k10_val.yaml
+
+      #Port forward
+      echo "--------------------Port Forward--------------------"
+      kubectl --namespace kasten-io port-forward service/gateway 8080:8000 &
+
+      #open kasten on brwoser
+      xdg-open http://127.0.0.1:8080/k10/
+    
     fi
 
-fi
+fi 
