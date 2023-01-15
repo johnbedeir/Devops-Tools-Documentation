8aee1409a13084a0925aa6260dde66ea756fd921
JohnyDev <john.bedeir@gmail.com>
2023-01-16 00:06:12 +0100

    Fix #188 create-aks-azure-terraform

---
+++ Terraform/Microsoft-Azure/AKS/aks.tf
@@ -0,0 +1,33 @@
+resource "azurerm_resource_group" "default" {
+  name     = "intax-rg"
+  location = "uaenorth"
+
+  tags = {
+    environment = "Demo"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "intax-aks"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  dns_prefix          = "intax-k8s"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_B2s"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = var.appId
+    client_secret = var.password
+  }
+
+  role_based_access_control_enabled = true
+
+  tags = {
+    environment = "Demo"
+  }
+}
---
+++ Terraform/Microsoft-Azure/AKS/versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.0.0"
+    }
+  }
+
+  required_version = ">= 0.14"
+}
