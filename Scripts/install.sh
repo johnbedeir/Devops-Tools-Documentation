60f4db4c9ac86b9daef6b1464ac80e6af35cb37e
MinaBottros001 <mina.bottros001@gmail.com>
2023-01-30 21:25:43 +0000

    update Java and python version

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -2,14 +2,14 @@
 echo "--------------------Installing Java--------------------"
 sudo apt-get update -y
 sudo apt upgrade -y 
-sudo apt-get install openjdk-8-jdk -y
+sudo apt-get install openjdk-17-jdk -y
 #Install Python
 echo "--------------------Installing Python--------------------"
 sudo apt-get update -y
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update -y
-sudo apt-get install python3.8 -y 
+sudo apt-get install python3.9 -y 
 #Install Pip
 echo "--------------------Installing Pip--------------------"
 sudo apt-get update -y
