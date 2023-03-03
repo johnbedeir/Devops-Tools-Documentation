97c6af767602649324d93cc2001498bc29066968
johnbedeir <john.bedeir@gmail.com>
2023-03-03 12:46:59 +0100

    Fix #218 update-monitoring-proj-with-argocd

--- DevOps-Pipeline-Monitoring/install.sh
+++ DevOps-Pipeline-Monitoring/install.sh
@@ -2,18 +2,7 @@
 echo "--------------------Installing Java--------------------"
 sudo apt-get update -y
 sudo apt upgrade -y 
-sudo apt-get install openjdk-8-jdk -y
-#Install Python
-echo "--------------------Installing Python--------------------"
-sudo apt-get update -y
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update -y
-sudo apt-get install python3.8 -y
-#Install Pip
-echo "--------------------Installing Pip--------------------"
-sudo apt-get update -y
-sudo apt-get install python3-pip -y 
+sudo apt install openjdk-11-jdk -y
 #Install Jenkins 
 echo "--------------------Installing Jenkins--------------------"
 sudo apt -y install wget
@@ -57,25 +46,17 @@ sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/
 sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(<kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-#Install Minikube
-echo "--------------------Installing Minikube--------------------"
-sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
 #Install K9s
 echo "--------------------Installing K9s--------------------"
 wget https://github.com/derailed/k9s/releases/download/v0.25.18/k9s_Linux_x86_64.tar.gz
 sudo tar -xvzf k9s_Linux_x86_64.tar.gz
 sudo install -m 755 k9s /usr/local/bin
-echo "--------------------Add Docker to Sudo group--------------------"
-sudo groupadd docker && sudo usermod -aG docker $USER && newgrp docker && sudo chmod 777 /var/run/docker.sock
 #Install AWS-CLI
 echo "--------------------Installing AWS-CLI--------------------"
 sudo apt-get install zip -y
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip
 sudo ./aws/install
-#Start Minikube 
-echo "--------------------Start Minikube--------------------"
-minikube start
-#Reveal Jenkins Password
+echo "--------------------Add Docker to Sudo group--------------------"
+sudo groupadd docker && sudo usermod -aG docker $USER && newgrp docker && sudo chmod 777 /var/run/docker.sock
 echo "--------------------Jenkins Password--------------------"
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
