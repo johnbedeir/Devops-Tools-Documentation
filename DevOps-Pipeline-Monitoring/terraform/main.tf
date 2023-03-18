ba82b90c3a6d774ed3080f27019b4eef06242539
JohnyDev <john.bedeir@gmail.com>
2023-03-18 18:08:04 +0100

    Fix #224 update-jenkinsfile

--- DevOps-Pipeline-Monitoring/terraform/main.tf
+++ DevOps-Pipeline-Monitoring/terraform/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "ubuntu-instance" {
-  ami           = var.ami
-  instance_type = "t2.medium"
-  key_name      = "mykey"
+  ami             = var.ami
+  instance_type   = "t2.medium"
+  key_name        = "mykey"
   security_groups = ["${aws_security_group.UbuntuSG.name}"]
 
-  tags  = {
-    Name  = "Ubuntu-EC2"
+  tags = {
+    Name = "Jenkins-EC2"
   }
 
   root_block_device {
@@ -14,6 +14,22 @@ resource "aws_instance" "ubuntu-instance" {
     encrypted             = true
     delete_on_termination = true
   }
+
+  # This section is to provision the EC2 and install a script file while building the EC2,
+  # due to interactions that has to be made while the installation is processing this sections will not be the best for this project
+
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "curl -O https://github.com/johnbedeir/Devops-Tools-Documentation/blob/dev/DevOps-Pipeline-Monitoring/install.sh",
+  #     "chmod +x install.sh",
+  #     "./install.sh"
+  #   ]
+
+  #   connection {
+  #     type        = "ssh"
+  #     user        = "ubuntu"
+  #     private_key = file("./mykey.pem")
+  #     host        = aws_instance.ubuntu-instance.public_ip
+  #   }
+  # }
 }
-
-
