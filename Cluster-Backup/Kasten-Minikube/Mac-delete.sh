48a1b61c462e7963abbf2589dce4269e1afd4311
MinaBottros001 <mina.bottros001@gmail.com>
2023-04-18 11:35:09 +0000

    #243 update Mac-delete.sh file

--- Cluster-Backup/Kasten-Minikube/Mac-delete.sh
+++ Cluster-Backup/Kasten-Minikube/Mac-delete.sh
@@ -2,10 +2,14 @@
 
 export NAMESPACE=kasten-io
 
+#Delete helm repository
+echo "--------------------Delete Helm Repo--------------------"
+helm repo remove kasten
+
 
 #Delete all resources in the namespace
 echo "--------------------Delete All Resources--------------------"
-kubectl delete namespace NAMESPACE --wait=false
+kubectl delete all --all -n $NAMESPACE
 
 #Delete namespace
 echo "--------------------Delete Namespace--------------------"
