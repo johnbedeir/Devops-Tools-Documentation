b39c04d607886d7f557a7655d3073e687a9a0882
JohnyDev <john.bedeir@gmail.com>
2023-09-30 02:15:54 +0200

    add-vars-argo-eks

--- Argo-CD/Argocd-EKS/remove-argocd-eks.sh
+++ Argo-CD/Argocd-EKS/remove-argocd-eks.sh
@@ -1,16 +1,24 @@
 # #/bin/bash
 
+CLUSTER_NAME=cluster-1
+NAMESPACE=argocd
+REGION=eu-central-1
+
 #Delete ArgoCD EKS
 echo "--------------------Delete ArgoCD EKS--------------------"
-kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl delete -n ${NAMESPACE} -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 #Delete argocd namespace
 echo "--------------------Deleting argocd namespace--------------------"
-kubectl delete ns argocd
+kubectl delete ns ${NAMESPACE}
 
 #Delete cluster
 echo "--------------------Deleting cluster--------------------"
-eksctl delete cluster --name cluster-1
+eksctl delete cluster --name ${CLUSTER_NAME} --region ${REGION}
+
+#Delete ArgoCD Password
+echo "--------------------Delete ArgoCD Password--------------------"
+rm -rf argo-pass.txt
 
 #Sleep 30 seconds
 echo "--------------------Wait for completely delete EKS--------------------"
