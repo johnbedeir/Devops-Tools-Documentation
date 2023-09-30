e931341108cf9dac93b962d69bb99201e77bf21a
JohnyDev <john.bedeir@gmail.com>
2023-09-30 02:23:10 +0200

    Dev (#320)
    * rmv-helm-repo
    
    * update-comingsoon-page-test-1
    
    * update-replicas-test
    
    * revert-test
    
    * revert-deploy
    
    * revert
    
    * deployment-updates
    
    * update-argo-minikube-script
    
    * update-argo-minikube-rmv-script
    
    * add-vars-argo-eks

--- Argo-CD/Argocd-EKS/deploy-argocd-eks.sh
+++ Argo-CD/Argocd-EKS/deploy-argocd-eks.sh
@@ -1,20 +1,25 @@
 # #/bin/bash
 
+CLUSTER_NAME=cluster-1
+NODES_NUMBER="2"
+NAMESPACE=argocd
+REGION=eu-central-1
+
 #Create cluster
 echo "--------------------Creating cluster--------------------"
-eksctl create cluster --name cluster-1 --nodes-min=2
+eksctl create cluster --name ${CLUSTER_NAME} --nodes-min=${NODES_NUMBER}
 
 #Update kubeconfig
 echo "--------------------Update kubeconfig--------------------"
-aws eks update-kubeconfig --name cluster-1
+aws eks update-kubeconfig --name ${CLUSTER_NAME} --region ${REGION}
 
 #Create argocd namespace
 echo "--------------------Creating argocd namespace--------------------"
-kubectl create ns argocd || true
+kubectl create ns ${NAMESPACE} || true
 
 #Deploy ArgoCD on EKS
 echo "--------------------Deploy ArgoCD on EKS--------------------"
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n ${NAMESPACE} -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 #Sleep 1 miniute
 echo "--------------------Wait for the pods to start--------------------"
@@ -22,13 +27,17 @@ sleep 1m
 
 #Change to LoadBalancer
 echo "--------------------Change Argocd Service to LoadBalancer--------------------"
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl patch svc argocd-server -n ${NAMESPACE} -p '{"spec": {"type": "LoadBalancer"}}'
+
+#Sleep 10 seconds
+echo "--------------------Creating External-IP--------------------"
+sleep 10s
 
 #Reveal Argocd URL
 echo "--------------------Argocd Ex-URL--------------------"
-kubectl get service argocd-server -n argocd | awk '{print $4}'
+kubectl get service argocd-server -n ${NAMESPACE} | awk '{print $4}'
 
 #Reveal ArgoCD Pass
 echo "--------------------ArgoCD UI Password--------------------"
 echo "Username: admin"
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo ' '
+kubectl -n ${NAMESPACE} get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > argo-pass.txt
