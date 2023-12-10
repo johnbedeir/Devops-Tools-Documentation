74ee21e31911ea9d5e67145f23f5d4def709eda8
JohnyDev <john.bedeir@gmail.com>
2023-12-10 10:51:27 +0100

    automate-Kubeconfig-github-secrets (#330)
    * rmv-helm-repo
    
    * update-comingsoon-page-test-1
    
    * update-replicas-test
    
    * revert-test
    
    * revert-deploy
    
    * revert
    
    * deployment-updates
    
    * update-argo-minikube-script
    
    * update-argo-minikube-rmv-script
    
    * add-vars-argo-eks
    
    * duplicate-node-app-eks-version
    
    * duplicate-node-app-dockerfile
    
    * k8s-eks-manifest-files
    
    * tf-eks-ingress-monitoring
    
    * automate-project-build-deployment
    
    * automate-project-destroy
    
    * Fix #323 kubeconfig-github-secrets
    
    * Fix #322 aws-github-secrets
    
    * update-vars-argo-eks
    
    * update-vars-argo-eks
    
    * update-ubuntu-gcp
    
    * Fix #327 EKS-RDS
    
    * Fix #327 EKS-RDS
    
    * Fix #329

--- Scripts/kubeconfig_github_secrets.sh
+++ Scripts/kubeconfig_github_secrets.sh
@@ -1,37 +1,18 @@
 #!/bin/bash
 
 # Define variables
-SECRET_NAME=""
+SECRET_NAME="KUBECONFIG"
 REPO_OWNER=""
 REPO_NAME=""
-KUBECONFIG_PATH=""
+KUBECONFIG_PATH="$HOME/.kube/config"
+KUBECONFIG=$(cat $KUBECONFIG_PATH | base64)
 
-# Check if kubeconfig file exists
-if [ ! -f "$KUBECONFIG_PATH" ]; then
-    echo "Error: kubeconfig file not found at $KUBECONFIG_PATH"
-    exit 1
-fi
 
-# Encode kubeconfig in base64
-ENCODED_KUBECONFIG=$(cat "$KUBECONFIG_PATH" | base64)
-
-echo "$ENCODED_KUBECONFIG"
-
-# Check if gh is installed
-if ! command -v gh &> /dev/null; then
-    echo "Error: gh command not found. Please install GitHub CLI."
-    exit 1
-fi
-
-# Authenticate with GitHub (ensure you are already logged in)
-# You may need to run `gh auth login` if you haven’t authenticated before
-
-# Create or update GitHub secret
-gh secret set "$SECRET_NAME" --repo="$REPO_OWNER/$REPO_NAME" --body="$ENCODED_KUBECONFIG"
-
-# Check the exit status of the last command and output appropriate message
-if [ $? -eq 0 ]; then
-    echo "Secret $SECRET_NAME successfully created or updated in $REPO_OWNER/$REPO_NAME"
-else
-    echo "Failed to create or update secret $SECRET_NAME in $REPO_OWNER/$REPO_NAME"
-fi
+# Add kubeconfig in GitHub secret
+echo $KUBECONFIG | gh secret set $SECRET_NAME --repos=$OWNER/$REPO
+    if [ $? -eq 0 ]; then
+        echo "Secret $SECRET_NAME set successfully."
+    else
+        echo "Failed to set secret $SECRET_NAME."
+        exit 1
+    fi
